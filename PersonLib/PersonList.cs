755d6fc29f08ffe6ff3f6176044d87599a76447d
nik79 <otrok2@mail.ru>
2021-12-18 18:09:13 +0700

    feature (Lab1): создан класс Person, начало создания класса PersonList

---
+++ PersonLib/PersonList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLib
+{
+    /// <summary>
+    /// Список людей
+    /// </summary>
+    public class PersonList
+    {
+        public Person[] personList = new Person[0];
+    }
+
+}
